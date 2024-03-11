90cf330954727ef08050bdaa650569df74740691
Yesh-x <162004041+Yesh-x@users.noreply.github.com>
2024-03-11 12:14:19 +0530

    Update app.py
    Updated the message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a demo python project!'
+    return 'TO check the workflow of the code'
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
